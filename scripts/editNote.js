92598a3122adb8bcd93fd75f54c10aa9a75de7d7
DmJsCourse <dimonua@gmail.com>
2021-10-07 15:39:43 +0300

    dates parsing, category display fix

--- scripts/editNote.js
+++ scripts/editNote.js
@@ -1,4 +1,36 @@
 import { displayNotes } from "./index.js";
+import { displayCategories } from "./index.js";
+
+const monthNames = [
+    "January", 
+    "February", 
+    "March", 
+    "April", 
+    "May", 
+    "June",
+    "July", 
+    "August", 
+    "September", 
+    "October", 
+    "November", 
+    "December"
+];
+
+function findDates(text) {
+    let foundDates = [...text.matchAll(/(\d{1,2})[\/\-\.](\d{1,2})[\/\-\.](\d{2,4})/g)];
+    return foundDates.map(date => { 
+        if (date[1].length === 1) {
+            date[1] = "0" + date[1];
+        };
+        if (date[2].length === 1) {
+            date[2] = "0" + date[2];
+        };
+        if (date[3].length === 2) {
+            date[3] = "20" + date[3];
+        };
+       return date[1] + '/' + date[2] + '/' + date[3];
+    }).join(', ');
+}
 
 export function editNote(itemId, list) {
     const overlay = document.querySelector('.inputs'); 
@@ -24,26 +56,33 @@ export function editNote(itemId, list) {
     submitButton.addEventListener('click', function(event){
         event.preventDefault();
         if(!itemId) {
+            let timeStamp = monthNames[creationTime.getMonth()] + 
+                ` ` + creationTime.getDate() + 
+                `, ` + creationTime.getFullYear()
+            ;
+
             list.push({
                 id: list[list.length-1].id + 1,
                 state: "active",
                 name: name.value, 
-                created: creationTime.getMonth() + `,` + creationTime.getFullYear(), 
+                created: timeStamp, 
                 category: category.value, 
                 content: content.value, 
-                dates: "",
+                dates: findDates(content.value),
             })
+            console.log(findDates(content.value))
         } else {
             let editedItem = list.find(entry => entry.id === itemId);
             editedItem.state = "active";
             editedItem.name = name.value;
             editedItem.category = category.value;
             editedItem.content = content.value;
-            editedItem.dates = "";
+            editedItem.dates = findDates(content.value);
         }
 
     overlay.classList.add('hidden');
     document.getElementsByClassName('notes__list')[0].innerHTML = '';
     displayNotes(list);
+    displayCategories(list);
     }, { once: true });
 }
--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,8 @@ let notes = [
     {id: 2, state: "active", name: "New feature", created: "May 05, 2021", category: "Idea", content: "Implement new feature 5/5/2021", dates: "5/5/2021"},
     {id: 3, state: "active", name: "William Gaddis", created: "May 07, 2021", category: "Task", content: "Lorem Ipsum", dates: ""},
     {id: 4, state: "active", name: "Books", created: "May 15, 2021", category: "Idea", content: "The Lean Startup", dates: ""},
-    {id: 5, state: "active", name: "Books", created: "May 16, 2021", category: "Random Thought", content: "The Lean Startup 2", dates: "16/5/2021"},
-    {id: 6, state: "active", name: "Books", created: "May 17, 2021", category: "Task", content: "The Lean Startup 3", dates: ""}
+    {id: 5, state: "active", name: "Books", created: "May 16, 2021", category: "Random Thought", content: "16/5/2021 book", dates: "16/5/2021"},
+    {id: 6, state: "active", name: "Books", created: "May 17, 2021", category: "Task", content: "The Lean Startup 2", dates: ""}
 ];
 
 import { displayNote }  from "./displayNote.js";
@@ -22,12 +22,13 @@ export const displayNotes = (notesList) => {
         .forEach(note => notesTable[0].insertAdjacentHTML('beforeend', note));
 }
 
-const displayCategories = () => {
-    const allCategories = [...new Set(notes.map(note => note.category))];
+export const displayCategories = (notesList) => {
+    categoriesTable[0].innerHTML = '';
+    const allCategories = [...new Set(notesList.map(note => note.category))];
     allCategories.forEach(category => {
         let activeCount = 0;
         let archivedCount = 0;
-        for(let note of notes) {
+        for(let note of notesList) {
             if (note.category === category) {
                 switch (note.state) {
                     case 'active':
@@ -58,4 +59,4 @@ newNoteButton.addEventListener('click', () => {
 });
 
 displayNotes(notes);
-displayCategories();
+displayCategories(notes);
